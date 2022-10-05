23dfeee10fdfdb2de10f08809806362e2c8ebd3c
Tony(Dohoon) Kim <dohoonk.smg@gmail.com>
2022-10-05 13:54:29 -0700

    WIP add testing for github_events endpoint

---
+++ spec/request/github_events_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe 'GitHubEvents', type: :request do
+    describe 'POST /github_events' do
+        context 'with valid parameters' do
+			let!(:github_event) { FactoryBot.create(:github_event) }
+            
+            before do
+                post '/api/v1/github_events', params: { user_id: github_event.user.id }
+            end
+
+            it 'returns a created status' do
+                expect(response).to have_http_status(:created)
+            end
+        end
+    end
+
+end
