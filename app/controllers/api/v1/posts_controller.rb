4d32b24c39265125fb188c7bf607c9fe24b7bcad
Tony(Dohoon) Kim <dohoonk.smg@gmail.com>
2022-10-04 23:04:01 -0700

    Add posts controller

---
+++ app/controllers/api/v1/posts_controller.rb
@@ -0,0 +1,17 @@
+class Api::V1::PostsController < ApplicationController
+    def create
+        post = Post.new(post_params)
+        
+        if post.save
+            render json: post, status: :created
+        else
+            render json: post.errors, status: :unprocessable_entity
+        end
+    end
+
+    private
+
+    def post_params
+		params.require(:post).permit(:title, :body, :user_id)
+	end
+end
