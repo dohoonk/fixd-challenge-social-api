f57bcb4fee25ace4c05e57bddbca70ed958cccb8
Tony(Dohoon) Kim <dohoonk.smg@gmail.com>
2022-10-05 01:33:12 -0700

    Add routes ratings

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
       resources :sessions, only: [:create, :destroy]
       resources :posts, only: [:create]
       resources :comments, only: [:create, :destroy]
+      resources :ratings, only: [:create]
     end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
