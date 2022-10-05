c5ede0f2f037257b6399ac94707fae8b40305a15
Tony(Dohoon) Kim <dohoonk.smg@gmail.com>
2022-10-05 01:19:31 -0700

    Refactor comments controller to move error handling logic to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
-class ApplicationController < ActionController::API
+class ApplicationController < ActionController::API   
+    rescue_from ActiveRecord::RecordNotDestroyed, with: :not_destroyed
+    rescue_from ActiveRecord::RecordNotFound, with: :not_found
+
+    def not_destroyed(e)
+        render json: { errors: e.record.errors }, status: :unprocessable_entity
+    end
+
+    def not_found(e)
+        render json: { error: e.to_s }, status: :not_found
+    end
 end
