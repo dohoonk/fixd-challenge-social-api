1771f56cc9414aec37bcbca419419e3375a797bf
Tony(Dohoon) Kim <dohoonk.smg@gmail.com>
2022-10-05 13:53:42 -0700

    Add github_events create controller to generate github_events from github public events api

---
+++ app/controllers/api/v1/github_events_controller.rb
@@ -0,0 +1,46 @@
+class Api::V1::GithubEventsController < ApplicationController
+
+    def create
+        user = User.find(params[:user_id])
+        request = Faraday.get("https://api.github.com/users/#{user.github_username}/events/public")
+        payloads   = JSON.parse(request.body)
+        payloads.each do |payload|
+            event_type = payload["type"]
+            case event_type
+                when "CreateEvent"
+                    event_id = payload["id"]
+                    repo_name = payload["repo"]["name"]
+                    created_at = payload["created_at"]
+                    github_event = GithubEvent.create(event_type: event_type, event_id: event_id, repo_name: repo_name, event_created_at: created_at, user_id: 1)
+                    
+                when "PushEvent"
+                    event_id = payload["id"]
+                    size = payload["payload"]["size"]
+                    repo_name = payload["repo"]["name"]
+                    created_at = payload["created_at"]
+                    github_event = GithubEvent.create(event_type: event_type, event_id: event_id, size: size, repo_name: repo_name, event_created_at: created_at, user_id: 1)
+
+                when "PullRequestEvent"
+                    event_id = payload["id"]
+                    status = payload["payload"]["action"]
+                    number = payload["payload"]["number"]
+                    repo_name = payload["repo"]["name"]
+                    created_at = payload["created_at"]
+                    github_event = GithubEvent.create(event_type: event_type, event_id: event_id, status: status, number: number, repo_name: repo_name, event_created_at: created_at, user_id: 1)
+
+                else
+                    puts "Oooh, something new from GitHub: #{event_type}"
+                end
+                
+        end
+
+    
+    end
+
+    private
+
+    def github_event_params
+        params.require(:github_event).permit(:event_type,:event_id, :repo_name, :event_created_at, :status, :size, :number, :user_id)
+    end
+
+end
