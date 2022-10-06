ff8349c0e0e25af3f2099a3b946cdad892a366d7
Tony(Dohoon) Kim <dohoonk.smg@gmail.com>
2022-10-06 00:13:37 -0700

    Fix post model validation to include title and body

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,6 +5,10 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :comments
 
+  # Validations
+  validates :title, presence: true
+  validates :body, presence: true
+  
   after_create :create_activity_log
   
 end
