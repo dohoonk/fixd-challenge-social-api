de172dff1ea71da97242821a914b6bfaa4569563
Tony(Dohoon) Kim <dohoonk.smg@gmail.com>
2022-10-06 00:21:31 -0700

    Add validation testing for post model

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,18 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# RSpec.describe Post, type: :model do
-#   pending "add some examples to (or delete) #{__FILE__}"
-# end
+RSpec.describe Post, type: :model do
+  let!(:post) { FactoryBot.create(:post) }
+
+  describe "validation" do
+    it "Should not let post be created without title" do
+      post.title = nil
+      expect(post).to_not be_valid
+    end
+
+    it "Should not let post be created without body" do
+      post.body = nil
+      expect(post).to_not be_valid
+    end
+
+  end
+end
