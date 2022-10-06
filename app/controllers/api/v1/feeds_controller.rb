d0baf0afe72f5e9ba41e3f4507cfbb4883726a63
Tony(Dohoon) Kim <dohoonk.smg@gmail.com>
2022-10-06 01:21:11 -0700

    Fix feeds controller to pull github_events data

--- app/controllers/api/v1/feeds_controller.rb
+++ app/controllers/api/v1/feeds_controller.rb
@@ -1,5 +1,9 @@
 class Api::V1::FeedsController < ApplicationController
     def index
+        # below method call will slow down the endpoint. Most ideal workaround would be to create
+        # a scheduled job that pulls from Github Api every 5 mins as github documentation
+        # states that their data is delayed by 5 mins.
+        GithubEventCreator.new(params[:user_id]).call
         @user = User.find(params[:user_id])
         render 'feeds/index', formats: [:json], handlers: [:jbuilder], status: 201
     end
