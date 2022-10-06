2daa4031504f68869fc69a67954e1dacc29af754
Tony(Dohoon) Kim <dohoonk.smg@gmail.com>
2022-10-06 01:42:08 -0700

    fix posts routes to have show action

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :sessions, only: [:create, :destroy]
-      resources :posts, only: [:create]
+      resources :posts, only: [:create, :show]
       resources :comments, only: [:create, :destroy]
       resources :ratings, only: [:create]
       resources :feeds, only: :index
