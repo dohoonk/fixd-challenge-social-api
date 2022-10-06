053c7eeb04c4e474469efad86b6031c6e109f3ba
Tony(Dohoon) Kim <dohoonk.smg@gmail.com>
2022-10-05 17:40:47 -0700

    Fix average_rating_creator to create activity_logs when user rating moves from below 4 to above 4

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_05_205800) do
+ActiveRecord::Schema.define(version: 2022_10_05_234917) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -80,6 +80,7 @@ ActiveRecord::Schema.define(version: 2022_10_05_205800) do
     t.datetime "updated_at", precision: 6, null: false
     t.decimal "average_rating", default: "0.0"
     t.string "github_username"
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
