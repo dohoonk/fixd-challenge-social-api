ac719931e7622acf4e3253b912212dd0d84087f1
Tony(Dohoon) Kim <dohoonk.smg@gmail.com>
2022-10-05 17:32:58 -0700

    Add activity_log_creater to handle activitylog model business logic

--- app/services/activity_log_creator.rb
+++ app/services/activity_log_creator.rb
@@ -11,10 +11,25 @@ class ActivityLogCreator
     private 
 
     def create_activity_log
-        ActivityLog.create(
-            subject: github_event,
-            name: "Created a new repo #{repo_name}",
-            user: user
-          )
+      
+         ActivityLog.create(
+            subject: @subject,
+            name: find_name,
+            user_id: @user_id
     end
+
+    def find_name
+        case @subject.class.name
+        when "Comment"
+           @name = "Commented on a post by #{@subject.user.name}"
+        when "Post"
+            @name = "#{@subject.title}"
+        when "Rating"
+            @name = "Passed 4 stars!"
+        when "GithubEvent"
+            @name = "Passed 4 stars!"
+        else
+            @name = "This is something new"
+        end
+    end 
 end
